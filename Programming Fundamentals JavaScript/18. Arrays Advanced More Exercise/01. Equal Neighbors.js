699794e47dcf1c645f5069e5a5100cd73d77bd76
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-06-13 13:26:01 +0300

    18. Arrays Advanced More Exercise

---
+++ Programming Fundamentals JavaScript/18. Arrays Advanced More Exercise/01. Equal Neighbors.js
@@ -0,0 +1,17 @@
+function equalNeighbors(input) {
+  for (let i = 0; i < input.length; i++) {
+    let current = input[i];
+    for (let j = 0; j < current.length; j++) {
+      let num = current[j];
+      for(let k = 0; k < current.length; k ++){
+          
+      }
+    }
+  }
+}
+equalNeighbors([
+  ["2", "3", "4", "7", "0"],
+  ["4", "0", "5", "3", "4"],
+  ["2", "3", "5", "4", "2"],
+  ["9", "8", "7", "5", "4"],
+]);
