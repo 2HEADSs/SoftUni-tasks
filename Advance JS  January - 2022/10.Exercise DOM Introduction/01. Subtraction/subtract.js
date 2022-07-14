dba32a2a06620e8db34d2123310e9412c30f0241
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-07-14 17:02:39 +0300

    10. DOM introduction - Exercise

---
+++ Advance JS  January - 2022/10.Exercise DOM Introduction/01. Subtraction/subtract.js
@@ -0,0 +1,6 @@
+function subtract() {
+  let firstNum = Number(document.getElementById('firstNumber').value);
+  let secondNum = Number(document.getElementById('secondNumber').value);
+  let result = document.getElementById('result');
+  result.textContent = firstNum - secondNum;
+}
---
+++ Advance JS  January - 2022/10.Exercise DOM Introduction/01. Subtraction/subtractOriginal.js
@@ -0,0 +1,9 @@
+// function subtract() {
+//   let num1 = document.getElementById('firstNumber');
+//   num1 = Number(num1.value);
+//   let num2 = document.getElementById('secondNumber');
+//   num2 = Number(num2.value);
+//   let result = num1 - num2;
+//   let resultElement = document.getElementById('result');
+//   resultElement.textContent = result;
+// }
