d1038a05bf7b62aa8bb75b5f566b45a7bdc186bd
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-08-01 18:22:58 +0300

    01. Area and Volume Calculator/2

---
+++ Advance JS  January - 2022/13. Advanced Functions - Lab/1.js
@@ -0,0 +1,27 @@
+function area() {
+  return Math.abs(this.x * this.y);
+}
+function vol() {
+  return Math.abs(this.x * this.y * this.z);
+}
+function solve(area, vol, input) {
+  let coordinates = JSON.parse(input);
+  let result = [];
+
+  for (let points of coordinates) {
+    result.push({
+      area: area.call(points),
+      volume: vol.call(points),
+    });
+  }
+  return result;
+}
+solve(
+  area,
+  vol,
+  `[
+{"x":"1","y":"2","z":"10"},
+{"x":"7","y":"7","z":"10"},
+{"x":"5","y":"2","z":"10"}
+]`
+);
