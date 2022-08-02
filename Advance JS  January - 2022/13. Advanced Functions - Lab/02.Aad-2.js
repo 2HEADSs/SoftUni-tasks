1e0836a6cf76b0fd721b16b02d780df407a29aff
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-08-02 13:37:01 +0300

    02.Aad-2

---
+++ Advance JS  January - 2022/13. Advanced Functions - Lab/02.Aad-2.js
@@ -0,0 +1,9 @@
+function solution(num){
+    function add(a,b){
+        return a+b
+    };
+    return add5.bind(this,number)
+}
+let add5 = solution(5);
+console.log(add5(2));
+console.log(add5(3));
