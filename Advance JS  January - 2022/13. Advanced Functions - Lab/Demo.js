7dce18bbc32e6adf3ca47dcde127006156eafd8e
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-08-08 09:05:03 +0300

    Demo

---
+++ Advance JS  January - 2022/13. Advanced Functions - Lab/Demo.js
@@ -0,0 +1,33 @@
+// function solve() {
+//   let person = {
+//     name: 'Pavel',
+//     age: 30,
+//     work: 'EOD',
+//   };
+//   let dataForMyName = 'name';
+
+//   console.log(person[dataForMyName]);
+// }
+// solve();
+
+let myObj = {
+  name: 'Pavel',
+  //няма  значение дали "property"-то го пишем с кавички
+  age: 29,
+  lastName: 'Dimitrov',
+  // стойността задължително с кавички ако е стринг
+};
+console.log(myObj);
+
+let dataForMe = 'age';
+//задаване на променлива която държи стринг идентичен с таакъв от масива
+console.log(myObj[dataForMe]);
+//при извикаване .... интерпретатора търси еднаква/равна променлива в обекта ,
+
+console.log(myObj['n' + 'ame']); // масив съдържащ стринг еднакъв с ключ от обекта
+//при извикаване .... интерпретатора търси еднаква/равна променлива(стринг) в обекта
+function getValue(propName) {
+  console.log(myObj[propName]);
+}
+getValue('lastName');
+getValue('name');
