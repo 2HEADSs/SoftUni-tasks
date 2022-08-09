15f991d71cbd4fb87951719f2df886d232f7414f
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-08-09 08:58:46 +0300

    closure-demo

---
+++ Advance JS  January - 2022/13. Advanced Functions - Lab/closure-demo.js
@@ -0,0 +1,21 @@
+function counterBuilder() {
+    let counter = 0;
+
+    return function c() {
+        counter++;
+        console.log(counter);
+    }
+}
+
+let counter = counterBuilder();
+let counter2 = counterBuilder();
+counter();
+counter();
+counter();
+counter();
+
+counter2();
+// counterBuilder()();
+// counterBuilder()();
+// counterBuilder()();
+// counterBuilder()();
