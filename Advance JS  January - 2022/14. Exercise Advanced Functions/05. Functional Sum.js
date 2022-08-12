de1d2b5e8ba8918da7e71b6a07917b5bd0b0030b
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-08-12 11:53:53 +0300

    05. Functional Sum

---
+++ Advance JS  January - 2022/14. Exercise Advanced Functions/05. Functional Sum.js
@@ -0,0 +1,13 @@
+function add(num) {
+  let sum = 0;
+  function inner(number) {
+    sum += number;
+    return inner;
+  }
+  inner.toString = () => {
+    return sum;
+  };
+  return inner(num);
+}
+add(1);
+add(1)(6)(-3);
