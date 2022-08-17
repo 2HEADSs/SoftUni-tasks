7f6dd84654e391a6261a5078518a0e4c4e2ff436
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-08-17 23:33:53 +0300

    1-sub-sum

---
+++ Advance JS  January - 2022/15.Unit Testing and Error Handling - Lab/1-sub-sum.js
@@ -0,0 +1,16 @@
+function solve(numbers, start, end) {
+    if (!Array.isArray(numbers)) {
+        return NaN;
+    }
+
+    let startIndex = Math.max(start, 0);
+    let endIndex = Math.min(end, numbers.length - 1);
+
+    let subNumbers = numbers.slice(startIndex, endIndex + 1);
+
+    let sum = subNumbers.reduce((a, x) => a + Number(x), 0);
+
+    return sum;
+}
+
+module.exports = solve;
