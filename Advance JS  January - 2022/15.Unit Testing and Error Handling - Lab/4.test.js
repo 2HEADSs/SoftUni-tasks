e2b67946c43340746e5d610dc9a4830099579ed6
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-08-18 08:56:39 +0300

    Create 4.test.js

---
+++ Advance JS  January - 2022/15.Unit Testing and Error Handling - Lab/4.test.js
@@ -0,0 +1,16 @@
+// const sum = require('./4');
+// const { assert } = require('chai');
+
+// describe('sum function test', () => {
+//   //correct answer
+//   it('should return sum of numbers in array', () => {
+//     assert(sum([5, 5, 5]) === 15);
+//   });
+//   it('should return sum of numbers in array', () => {
+//     assert(sum([-5, 5.5, 5.5]) === 6);
+//   });
+//   it('should return sum of numbers in array', () => {
+//     assert(sum([-5, -5, -5]) === -15);
+//   });
+  
+// });
