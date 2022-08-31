5f4f27120811746833742366fdc094a07523973f
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-08-31 13:07:54 +0300

    Even-or-odd

---
+++ Advance JS  January - 2022/16. Exercise Unit Testing and Error Handling/02. Even Or Odd.test.js
@@ -0,0 +1,25 @@
+// const isOddOrEven = require('./02. Even Or Odd.js');
+// const { assert } = require('chai');
+
+// describe('isOddOrEven function tests', () => {
+//   //invalid input tests
+//   it('should return undefined if the parameter is number', () => {
+//     assert.equal(isOddOrEven(10), undefined);
+//   });
+//   it('should return undefined if the parameter is object', () => {
+//     assert.equal(isOddOrEven({}), undefined);
+//   });
+//   it('should return undefined if the parameter is array', () => {
+//     assert.equal(isOddOrEven([]), undefined);
+//   });  
+//   it('should return undefined if the parameter is undefiined', () => {
+//     assert.equal(isOddOrEven(undefined), undefined);
+//   });
+//   //valid input tests
+//   it('should return even if the parameters length is even', () => {
+//     assert.equal(isOddOrEven('Hi'), 'even');
+//   });
+//   it('should return odd if the parameters length is odd', () => {
+//     assert.equal(isOddOrEven('Hello'), 'odd');
+//   });
+// });
