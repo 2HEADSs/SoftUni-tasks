a80e358c2dba9fd4a2a463b809caadd60e4d6484
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-09-04 16:35:30 +0300

    Create 2.test.js

---
+++ Advance JS  January - 2022/16. Exercise Unit Testing and Error Handling/2.test.js
@@ -0,0 +1,19 @@
+// const isOddOrEven = require('./2');
+// const { assert } = require('chai');
+
+// describe('isOddOrEven function tests', () => {
+//   //incorrect inputs
+//   it('should return undefined if parameter is not a string', () => {
+//     assert.equal(isOddOrEven(10), undefined);
+//   });
+//   it('should return undefined if parameter is not a string', () => {
+//     assert.equal(isOddOrEven({}), undefined);
+//   });
+//   //correct inputs
+//   it('should return even if parameter"s length is even', () => {
+//     assert.equal(isOddOrEven('Love'), 'even');
+//   });
+//   it('should return odd if parameter"s length is odd', () => {
+//     assert.equal(isOddOrEven('Animals'), 'odd');
+//   });
+// });
