89467b5c4f69835299762ff3adc840e006b9c6ac
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-09-10 11:33:32 +0300

    accesor-property

---
+++ Advance JS  January - 2022/17.Classes/accesor-property.js
@@ -0,0 +1,11 @@
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+        this.diameter = this.radius * 2
+    }
+}
+let circle = new Circle(2);
+console.log(circle);
+
+let circle2 = new Circle(4);
+console.log(circle2);
