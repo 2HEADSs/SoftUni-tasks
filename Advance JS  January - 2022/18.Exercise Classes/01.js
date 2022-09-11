d79d631c010a0dfaa1d279b51b25a9876abcc70c
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-09-11 15:01:15 +0300

    Create 01.js

---
+++ Advance JS  January - 2022/18.Exercise Classes/01.js
@@ -0,0 +1,19 @@
+class Rectangle {
+  constructor(width, height, color) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  calcArea() {
+    return this.width * this.height;
+  }
+}
+// Write a class Rectangle for a rectangle object. It needs to have a width (Number),
+// height (Number), and color (String) properties, which are set from the constructor,
+//  and a calcArea() method, that calculates and returns the rectangle’s area
+let rect = new Rectangle(4, 5, 'Red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());
