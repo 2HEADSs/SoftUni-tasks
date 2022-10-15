028b224ae1458ea4b24fd4c6210a201e3da609a8
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-10-15 14:18:05 +0300

    02. Christmas Movies

---
+++ Advance JS  January - 2022/Exams/JS Advanced Retake Exam - 10 December 2019/02. Christmas Movies/02. Christmas Movies_Resources.test.js
@@ -0,0 +1,8 @@
+const ChristmasMovies = require('./02. Christmas Movies_Resources');
+const { expect } = require('chai');
+
+describe('ChristmasMovies functionality test', () => {
+    describe('buyMovie', () => {
+        it()
+    })
+})
